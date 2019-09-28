5a78c18c4157d84bcbc519b67a22b0ff106ca1ee
Dominik Schöni <dominik@dominikschoeni.ch>
2019-09-28 22:24:07 +0200

    stuff

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import '@/custom.scss'
 
 Vue.use(BootstrapVue)
 
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(faCircleNotch)
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
