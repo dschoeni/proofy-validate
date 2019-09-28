b7ec5b3b3a299969d18b1b41c7ac2e9123a3dfe3
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2019-09-28 22:37:16 +0200

    feat(url): add url parameter for chrome extension

--- src/views/proof.vue
+++ src/views/proof.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-      <div class="d-flex flex-column align-items-center">
-        
-        <b-form-input v-model="url" placeholder="https://www.example.com"></b-form-input>
+    <div class="d-flex flex-column align-items-center">
+      <b-form-input v-model="url" placeholder="https://www.example.com"></b-form-input>
 
-        <div class="btn-group">
-          <button class="btn btn-primary my-4" :disabled="!url" @click="notarize(url)">Generate Proof</button>
-          <a class="btn btn-secondary my-4" :href="'data:' + downloadData" :class="{ 'disabled': !downloadData, 'btn-success': downloadData }" download="notarized.proof">Download Proof</a>
-        </div>
+      <div class="btn-group">
+        <button class="btn btn-primary my-4" :disabled="!url" @click="notarize(url)">Generate Proof</button>
+        <a
+          class="btn btn-secondary my-4"
+          :href="'data:' + downloadData"
+          :class="{ 'disabled': !downloadData, 'btn-success': downloadData }"
+          download="notarized.proof"
+        >Download Proof</a>
+      </div>
+    </div>
 
-        </div>
+    <hr />
 
-        <hr />
-
-        <Diff :proofs="proofs" />
+    <Diff :proofs="proofs" />
   </div>
 </template>
 
@@ -21,23 +24,34 @@
 import validationService from '@/services/validator'
 
 export default {
-  name: "Notarize",
+  name: 'Notarize',
   components: {
     Diff: () => import('@/views/diff.vue')
   },
+  mounted() {
+    if (this.$route.query.url) {
+      this.url = this.$route.query.url
+      this.notarize(this.url)
+    }
+  },
   methods: {
     async notarize(url) {
+      if(!url.startsWith('http')){
+        url = 'https://' + url
+      }
       this.proofs = []
 
-      await Promise.all(this.validators.map(async (validator) => {
-        const response = await fetch(`https://proof.viser.ch?url=${encodeURIComponent(url)}`)
-        const jsonAnswer = await response.json()
-        jsonAnswer.validator = validator.key
-        jsonAnswer.url = url
-        this.proofs.push(jsonAnswer)
-      }))
-      
-      this.downloadData = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.proofs));
+      await Promise.all(
+        this.validators.map(async validator => {
+          const response = await fetch(`https://proof.viser.ch?url=${encodeURIComponent(url)}`)
+          const jsonAnswer = await response.json()
+          jsonAnswer.validator = validator.key
+          jsonAnswer.url = url
+          this.proofs.push(jsonAnswer)
+        })
+      )
+
+      this.downloadData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(this.proofs))
     }
   },
   data() {
@@ -49,7 +63,7 @@ export default {
       proofs: []
     }
   }
-};
+}
 </script>
 
 <style scoped>
