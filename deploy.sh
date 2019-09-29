4df1fa2c0b915d4e3fc4dedb1d37ae0ce9dd0060
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2019-09-29 08:26:19 +0200

    fix(deploy): cname adjusted

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'validate.viser.ch' > CNAME
+echo 'veritas.viser.ch' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://dschoeni.github.io/proofy-validate
-git push -f git@github.com:dschoeni/proofy-validate.git master:gh-pages
+git push -f git@github.com:dschoeni/veritas-frontend.git master:gh-pages
 
 cd -
+
